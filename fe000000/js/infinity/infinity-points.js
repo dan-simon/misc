8e2d247a9cfe063d72aabed101a2daaf0497dad5
Dan Simon <dan@pipeline.com>
2019-11-23 20:21:23 -0500

    this is so slow, let's make it quicker. only about 3x quicker for a long time, then jumps up to more right about when it needs to

--- fe000000/js/infinity/infinity-points.js
+++ fe000000/js/infinity/infinity-points.js
@@ -7,10 +7,6 @@ let InfinityPoints = {
     player.stats.totalIPProduced = player.stats.totalIPProduced.plus(x);
   },
   multiplier() {
-    if (this.amount().gt(32)) {
-      return this.amount().log(2) * 2 - 1;
-    } else {
-      return this.amount().div(4).plus(1);
-    }
+    return this.amount().div(4).plus(1);
   }
 }
