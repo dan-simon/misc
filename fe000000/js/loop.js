cee10b43e4334cc21939781c3f4931e978d42a8f
Dan Simon <dan@pipeline.com>
2020-04-13 08:40:56 -0400

    Optimize production order

--- fe000000/js/loop.js
+++ fe000000/js/loop.js
@@ -4,17 +4,18 @@ function gameLoop(diff, display) {
     diff = (now - player.lastUpdate) / 1024 * player.cheats.gameSpeed;
     player.lastUpdate = now;
   }
+  // This order is (I think) the one giving the most stuff.
   Boost.produceBoostPower(diff);
-  if (!InfinityPrestigeLayer.mustInfinity()) {
-    for (let i = 8; i >= 1; i--) {
-      Generator(i).produce(diff);
-    }
+  for (let i = 8; i >= 1; i--) {
+    EternityGenerator(i).produce(diff);
   }
   for (let i = 8; i >= 1; i--) {
     InfinityGenerator(i).produce(diff);
   }
-  for (let i = 8; i >= 1; i--) {
-    EternityGenerator(i).produce(diff);
+  if (!InfinityPrestigeLayer.mustInfinity()) {
+    for (let i = 8; i >= 1; i--) {
+      Generator(i).produce(diff);
+    }
   }
   Stats.addToTimeStats(diff);
   InfinityPrestigeLayer.updatePeakIPPerSec();
