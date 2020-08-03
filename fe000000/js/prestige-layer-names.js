9042b97aefcf2d586db424426d616c79c3d62c3d
Dan Simon <des480@nyu.edu>
2020-08-03 19:32:41 -0400

    Fix various text issues

---
+++ fe000000/js/prestige-layer-names.js
@@ -0,0 +1,22 @@
+let PrestigeLayerNames = {
+  layers: ['prestige', 'infinity', 'eternity', 'complexity'],
+  highestLayer() {
+    if (player.complexities > 0) {
+      return 'complexity';
+    } else if (player.eternities.gt(0)) {
+      return 'eternity';
+    } else if (player.infinities > 0) {
+      return 'infinity';
+    } else {
+      return 'prestige';
+    }
+  },
+  layersAbove(x) {
+    let layers = this.layers.slice(this.layers.indexOf(x), this.layers.indexOf(this.highestLayer()) + 1);
+    if (layers.length < 3) {
+      return layers.join(' and ');
+    } else {
+      return layers.slice(0, -1).join(', ') + ', and ' + layers[layers.length - 1];
+    }
+  }
+}
