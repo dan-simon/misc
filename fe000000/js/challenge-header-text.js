6f5529206bf344975d12488af5a9273ffe590373
Dan Simon <des480@nyu.edu>
2020-08-13 22:11:15 -0400

    Remove CC1 from header

--- fe000000/js/challenge-header-text.js
+++ fe000000/js/challenge-header-text.js
@@ -1,11 +1,12 @@
 let ChallengeHeaderText = {
   getText() {
-    let runningComplexityChallenges = [1, 2, 3, 4, 5, 6].filter(x => ComplexityChallenge.isComplexityChallengeRunning(x));
+    // Don't include 1.
+    let runningComplexityChallenges = [2, 3, 4, 5, 6].filter(x => ComplexityChallenge.isComplexityChallengeRunning(x));
     let challenges = [
       Challenge.isSomeChallengeRunning() ? 'Challenge ' + Challenge.currentChallenge() : null,
       InfinityChallenge.isSomeInfinityChallengeRunning() ? 'Infinity Challenge ' + InfinityChallenge.currentInfinityChallenge() : null,
       EternityChallenge.isSomeEternityChallengeRunning() ? 'Eternity Challenge ' + EternityChallenge.currentEternityChallenge() : null,
-      (runningComplexityChallenges.length > 0 && PrestigeLayerProgress.hasReached('complexity')) ?
+      runningComplexityChallenges.length > 0 ?
       'Complexity Challenge' + pluralize(runningComplexityChallenges.length, '', 's') + ' ' + coordinate('*', '', runningComplexityChallenges) : null
     ];
     return coordinate('You are currently in *.', 'You are currently not in any challenge.', challenges);
