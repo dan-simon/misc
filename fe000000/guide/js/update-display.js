a8f1d72370aba9fbc2e0cdd0674a512dd70778cf
Dan Simon <des480@nyu.edu>
2020-08-14 10:29:18 -0400

    Add to guide

--- fe000000/guide/js/update-display.js
+++ fe000000/guide/js/update-display.js
@@ -2,7 +2,7 @@ let e;
 let b;
 
 function updateDisplayPageLoadSetup() {
-  e = [document.getElementById("e0"), document.getElementById("e1"), document.getElementById("e2"), document.getElementById("e3"), document.getElementById("e4"), document.getElementById("e5"), document.getElementById("e6"), document.getElementById("e7"), document.getElementById("e8"), document.getElementById("e9"), document.getElementById("e10"), document.getElementById("e11"), document.getElementById("e12"), document.getElementById("e13"), document.getElementById("e14"), document.getElementById("e15"), document.getElementById("e16"), document.getElementById("e17"), document.getElementById("e18"), document.getElementById("e19"), document.getElementById("e20"), document.getElementById("e21"), document.getElementById("e22"), document.getElementById("e23"), document.getElementById("e24"), document.getElementById("e25"), document.getElementById("e26"), document.getElementById("e27"), document.getElementById("e28"), document.getElementById("e29"), document.getElementById("e30"), document.getElementById("e31")];
+  e = [document.getElementById("e0"), document.getElementById("e1"), document.getElementById("e2"), document.getElementById("e3"), document.getElementById("e4"), document.getElementById("e5"), document.getElementById("e6"), document.getElementById("e7"), document.getElementById("e8"), document.getElementById("e9"), document.getElementById("e10"), document.getElementById("e11"), document.getElementById("e12"), document.getElementById("e13"), document.getElementById("e14"), document.getElementById("e15"), document.getElementById("e16"), document.getElementById("e17"), document.getElementById("e18"), document.getElementById("e19"), document.getElementById("e20"), document.getElementById("e21"), document.getElementById("e22"), document.getElementById("e23"), document.getElementById("e24"), document.getElementById("e25"), document.getElementById("e26"), document.getElementById("e27"), document.getElementById("e28"), document.getElementById("e29"), document.getElementById("e30"), document.getElementById("e31"), document.getElementById("e32"), document.getElementById("e33"), document.getElementById("e34"), document.getElementById("e35"), document.getElementById("e36"), document.getElementById("e37"), document.getElementById("e38"), document.getElementById("e39"), document.getElementById("e40"), document.getElementById("e41"), document.getElementById("e42"), document.getElementById("e43"), document.getElementById("e44"), document.getElementById("e45"), document.getElementById("e46"), document.getElementById("e47"), document.getElementById("e48"), document.getElementById("e49"), document.getElementById("e50"), document.getElementById("e51"), document.getElementById("e52"), document.getElementById("e53"), document.getElementById("e54"), document.getElementById("e55"), document.getElementById("e56"), document.getElementById("e57"), document.getElementById("e58"), document.getElementById("e59"), document.getElementById("e60"), document.getElementById("e61"), document.getElementById("e62"), document.getElementById("e63"), document.getElementById("e64"), document.getElementById("e65"), document.getElementById("e66"), document.getElementById("e67"), document.getElementById("e68"), document.getElementById("e69"), document.getElementById("e70"), document.getElementById("e71")];
   b = [];
 }
 
@@ -43,5 +43,45 @@ function updateDisplay() {
   e[29].textContent = formatInt(Math.pow(2, 256));
   e[30].textContent = formatInt(Math.pow(2, 248));
   e[31].textContent = formatInt(Math.pow(2, 256));
+  e[32].textContent = formatInt(2);
+  e[33].textContent = formatInt(1);
+  e[34].textContent = formatInt(2);
+  e[35].textContent = formatInt(4);
+  e[36].textContent = formatInt(1.5);
+  e[37].textContent = formatInt(2);
+  e[38].textContent = formatInt(2);
+  e[39].textContent = formatInt(2);
+  e[40].textContent = formatInt(2);
+  e[41].textContent = formatInt(1);
+  e[42].textContent = formatInt(2);
+  e[43].textContent = format(Math.sqrt(2));
+  e[44].textContent = formatInt(4);
+  e[45].textContent = format(2);
+  e[46].textContent = formatInt(4);
+  e[47].textContent = formatInt(3);
+  e[48].textContent = formatInt(3);
+  e[49].textContent = formatInt(0);
+  e[50].textContent = formatInt(1);
+  e[51].textContent = formatInt(4);
+  e[52].textContent = formatInt(2);
+  e[53].textContent = formatInt(5);
+  e[54].textContent = formatInt(4);
+  e[55].textContent = format(formatWithPrecision(1 / 256, 5));
+  e[56].textContent = formatInt(4);
+  e[57].textContent = formatInt(8);
+  e[58].textContent = formatInt(16);
+  e[59].textContent = formatInt(2);
+  e[60].textContent = formatInt(2048);
+  e[61].textContent = formatInt(5);
+  e[62].textContent = formatInt(6);
+  e[63].textContent = formatInt(6);
+  e[64].textContent = formatInt(5);
+  e[65].textContent = formatInt(11);
+  e[66].textContent = formatInt(16);
+  e[67].textContent = formatInt(8);
+  e[68].textContent = formatInt(2);
+  e[69].textContent = format(2.25);
+  e[70].textContent = formatInt(4);
+  e[71].textContent = formatInt(16);
 
 }
