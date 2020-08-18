dc66829e0e9d49641b6d14f59917f16481727243
Dan Simon <des480@nyu.edu>
2020-08-17 20:36:40 -0400

    It's a start

---
+++ fe000000/temp-template-changes.js
@@ -0,0 +1,9 @@
+let fs = require('fs');
+
+fs.readFile('index-template.html', 'utf8', function(err, contents) {
+  let r = /( +)<span ~class=Powers\.color\((['a-z]+), (\d+)\)~>~r Powers\.description\(['a-z]+, \d+\) ~<\/span>\n +<br\/>\n +<span ~class=Powers\.color\(['a-z]+, \d+\)~>~r Powers.details\(['a-z]+, \d+\) ~<\/span>/gm;
+  let newContents = contents.replace(r, function (match, x1, x2, x3) {
+    return `${x1}<span ~class=Powers.color(${x2}, ${x3})~>~r Powers.detailsFirstLine(${x2}, ${x3}) ~</span>\n${x1}<br/>\n${x1}<span ~class=Powers.color(${x2}, ${x3})~>~r Powers.detailsSecondLine(${x2}, ${x3}) ~</span>`;
+  });
+  fs.writeFile('new-index-template.html', newContents, function (err) {console.log(err)});
+});
