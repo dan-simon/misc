f0dede822bcb4f7f58fefc6ab762c851b0928c4d
Dan Simon <des480@nyu.edu>
2020-09-01 13:30:56 -0400

    Add to guide

--- fe000000/guide/js/main.js
+++ fe000000/guide/js/main.js
@@ -30,7 +30,7 @@ window.onload = function () {
 
 let randomMessageList = [
   'Do you really need a guide for this?',
-  'This guide currently covers only a small fraction of the game.',
+  'This guide does not yet cover the entire game.',
   'Nothing in here is necessarily optimal, but it\'s probably fairly close.',
   'Feel free to make suggestions to improve this guide.',
   'Did you know that some stars in the sky are dead, but we still see their ancient image?',
