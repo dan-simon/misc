ba2ed4f187722295f7f194be03049ee622e5c730
Dan Simon <des480@nyu.edu>
2020-10-02 23:03:30 -0400

    start adding to guide

--- fe000000/guide/js/tabs.js
+++ fe000000/guide/js/tabs.js
@@ -10,7 +10,7 @@ let Tabs = {
     return temporarySettings.viewAll || x === 1 || player.goals.slice(x - 2).some(x => x);
   },
   displayTabRow(i) {
-    return this.isTabVisible([1, 5, 7, 9, 11][i - 1]);
+    return this.isTabVisible([1, 5, 7, 9, 11, 13, 15][i - 1]);
   },
   setViewAll(x) {
     temporarySettings.viewAll = x;
