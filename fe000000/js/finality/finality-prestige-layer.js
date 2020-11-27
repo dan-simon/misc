6ea90d88534858655cc789e0c9b4b92a3ad7d758
Dan Simon <des480@nyu.edu>
2020-11-27 08:35:26 -0500

    Fix bug

--- fe000000/js/finality/finality-prestige-layer.js
+++ fe000000/js/finality/finality-prestige-layer.js
@@ -110,7 +110,7 @@ let FinalityPrestigeLayer = {
     player.boughtTheorems = [0, 0, 0];
     player.studies = initialStudies();
     player.studySettings.firstTwelveStudyPurchaseOrder = [];
-    player.studySettings.studiesBeforeLastRespec = initalStudies();
+    player.studySettings.studiesBeforeLastRespec = initialStudies();
     // Extra theorems seem to fit in best here, because they're theorem-related, even though
     // nothing resets them other than finality.
     player.extraTheorems = [0, 0, 0, 0];
