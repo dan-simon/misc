3816d8d2f3a7987da330e93b308f11a7c8520ac9
Dan Simon <des480@nyu.edu>
2021-03-16 08:08:45 -0400

    Fix default max all bug

--- fe000000/js/infinity/infinity-max-all.js
+++ fe000000/js/infinity/infinity-max-all.js
@@ -9,6 +9,9 @@ let InfinityMaxAll = {
     return InfinityGenerators.list.some(x => x.canBuy());
   },
   maxAll(types) {
+    if (types === undefined) {
+      types = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    }
     generalHighestSweep(() => InfinityGenerators.highest(), types);
     generalMaxAll(types.filter(x => x > 8).map(x => InfinityUpgrade(x - 8)));
     generalMaxAll(types.filter(x => x <= 8).map(x => InfinityGenerator(x)));
