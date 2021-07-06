e0617c62c41dabd52d247c99fbf943df2eee2b11
Dan Simon <des480@nyu.edu>
2021-07-06 12:54:04 -0400

    Add 'normal'

--- fe000000/js/challenge-header-text.js
+++ fe000000/js/challenge-header-text.js
@@ -3,7 +3,7 @@ let ChallengeHeaderText = {
     // Don't include 1.
     let runningComplexityChallenges = [2, 3, 4, 5, 6].filter(x => ComplexityChallenge.isComplexityChallengeRunning(x));
     let challenges = [
-      Challenge.isSomeChallengeRunning() ? 'Challenge ' + Challenge.currentChallenge() : null,
+      Challenge.isSomeChallengeRunning() ? 'Normal Challenge ' + Challenge.currentChallenge() : null,
       InfinityChallenge.isSomeInfinityChallengeRunning() ? 'Infinity Challenge ' + InfinityChallenge.currentInfinityChallenge() : null,
       EternityChallenge.isSomeEternityChallengeRunning() ? 'Eternity Challenge ' + EternityChallenge.currentEternityChallenge() : null,
       runningComplexityChallenges.length > 0 ?
