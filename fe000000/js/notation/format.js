1ffd5eecfb9b9d60987d0213cbadb54e538bdda2
Dan Simon <des480@nyu.edu>
2022-09-05 17:17:03 -0400

    Add autobuyer improvements

--- fe000000/js/notation/format.js
+++ fe000000/js/notation/format.js
@@ -29,19 +29,6 @@ function formatMaybeInt (x) {
   return Decimal.eq(x, Decimal.round(x)) ? formatInt(x) : format(x);
 }
 
-function notationIsLikeScientific(x) {
-  return ['Scientific', 'Logarithm', 'Engineering', 'Letters',
-  'Mixed Scientific', 'Mixed Engineering', 'Mixed Logarithm (Sci)'].includes(x.name);
-}
-
-function maybeAddInitialZero(x, expected, maybeDo) {
-  if (x.length === 1 && (x === expected || notationIsLikeScientific(getTimeNotation())) && maybeDo) {
-    return '0' + x;
-  } else {
-    return x;
-  }
-}
-
 function getTimeNotation() {
   return Options.notationOnTimes() ? getNotation() : getNotation('TimeScientific');
 }
@@ -60,18 +47,41 @@ function formatTimeMaybeInt(x) {
   return Decimal.eq(x, Decimal.round(x)) ? formatTimeInt(x) : formatTimeNum(x);
 }
 
+let digitsCache = {};
+
+let getNeededDigits = function (base) {
+  if (!(base in digitsCache)) {
+    digitsCache[base] = [null].concat([24, 60, 60].map(i => Math.ceil(Math.log(i) / Math.log(base))));
+  }
+  return digitsCache[base];
+}
+
+function maybeAddInitialZero(x, expected, digits) {
+  if (digits !== null && x.length < digits && (x === expected || getTimeNotation().isCommon)) {
+    return x.padStart(digits, '0');
+  } else {
+    return x;
+  }
+}
+
+function formatDHMS(time, base) {
+  let parts = [Math.floor(time / 86400), Math.floor(time / 3600) % 24, Math.floor(time / 60) % 60, Math.floor(time) % 60];
+  let digits = getNeededDigits(base);
+  while (parts[0] === 0) {
+    parts.shift();
+  }
+  let f = (x, i) => maybeAddInitialZero(formatTimeInt(x), x.toString(), (i === 0) ? null : digits[4 - parts.length + i]);
+  return parts.map(f).join(':');
+}
+
 function formatTime(time, options) {
   let timeDisplay = player.options.timeDisplay;
   if (timeDisplay === 'Seconds' || time < 60) {
     return options.seconds.f(time) + ' second' + (options.seconds.s ? pluralize(time, '', 's') : 's');
-  } else if (timeDisplay === 'D:H:M:S' || timeDisplay === 'D:H:M:S with notation') {
-    let applyNotation = (timeDisplay === 'D:H:M:S with notation');
-    let parts = [Math.floor(time / 86400), Math.floor(time / 3600) % 24, Math.floor(time / 60) % 60, Math.floor(time) % 60];
-    while (parts[0] === 0) {
-      parts.shift();
-    }
-    let f = (x, i) => maybeAddInitialZero(formatTimeInt(x), x.toString(), i !== 0);
-    return parts.map(f).join(':');
+  } else if (timeDisplay === 'D:H:M:S') {
+    // usesBase can be undefined but that's fine (we treat it as false).
+    let base = getTimeNotation().usesBase ? NotationOptions.displayBase() : 10;
+    return formatDHMS(time, base);
   } else if (timeDisplay === 'Largest unit') {
     if (time < 3600) {
       return options.larger.f(time / 60) + ' minute' + (options.larger.s ? pluralize(time / 60, '', 's') : 's');
@@ -107,9 +117,16 @@ function pluralize(x, singular, plural) {
   return Decimal.eq(x, 1) ? singular : plural;
 }
 
-function autobuyerSettingToString(x) {
+function isUsingTimeMode(autobuyer) {
+  return Autobuyer(autobuyer).mode().slice(0, 4) === 'Time';
+}
+
+function autobuyerSettingToString(x, autobuyer) {
+  let time = isUsingTimeMode(autobuyer);
+  let specialFormat = NotationOptions.parseAutobuyersInCurrentBase() && (!time || Options.notationOnTimes());
   let prec = NotationOptions.autobuyerPrecision();
-  if (NotationOptions.parseAutobuyersInCurrentBase()) {
+  if (specialFormat) {
+    // Actually formatting as DMHS is very annoying
     if (NotationOptions.notation() === 'Hex') {
       return formatWithPrecision(x, prec);
     } else {
@@ -120,17 +137,31 @@ function autobuyerSettingToString(x) {
   }
 }
 
-function stringToAutobuyerSetting(x) {
-  if (NotationOptions.notation() === 'Hex') {
+function stringToAutobuyerSetting(x, autobuyer) {
+  let time = isUsingTimeMode(autobuyer);
+  let specialFormat = NotationOptions.parseAutobuyersInCurrentBase() && (!time || Options.notationOnTimes());
+  let parts = x.split(':').map(i => stringToAutobuyerSettingNoColon(i, specialFormat));
+  let res = parts[0];
+  for (let i = 1; i < parts.length; i++) {
+    let unit = parts.length - 1 - i;
+    // I only know of one system that represents large times as arbitrarily long sequences of small numbers.
+    res = res.times((unit >= 5) ? 20 : [60, 60, 24, 20, 18][unit]);
+    res = res.plus(parts[1]);
+  }
+  return res;
+}
+
+function stringToAutobuyerSettingNoColon(x, specialFormat) {
+  if (specialFormat && NotationOptions.notation() === 'Hex') {
     return hexToNumber(x);
   }
-  let y = x.split(displayDigitsAutobuyerSettings().includes('E') ? 'e' : /[Ee]/g).map(i => i.toUpperCase());
-  if (y[y.length - 1] === '' && x[x.length - 1].toUpperCase() === 'E') {
+  let y = x.split(displayDigitsAutobuyerSettings(specialFormat).includes('E') ? 'e' : /[Ee]/g).map(i => i.toUpperCase());
+  if (y[y.length - 1] === '' && x && x[x.length - 1].toUpperCase() === 'E') {
     y.pop();
     y[y.length - 1] += 'E';
   }
   y = y.map(stringToAutobuyerSettingNoExponent);
-  return y.length > 0 ? y.reduceRight((a, b) => Decimal.pow(exponentBaseAutobuyerSettings(), a).times(b)) : new Decimal(0);
+  return y.length > 0 ? y.reduceRight((a, b) => Decimal.pow(exponentBaseAutobuyerSettings(specialFormat), a).times(b)) : new Decimal(0);
 }
 
 function hexToNumber(x) {
@@ -156,8 +187,8 @@ function hexToNumber(x) {
   return op;
 }
 
-function stringToAutobuyerSettingNoExponent(x) {
-  let digits = displayDigitsAutobuyerSettings();
+function stringToAutobuyerSettingNoExponent(x, specialFormat) {
+  let digits = displayDigitsAutobuyerSettings(specialFormat);
   let sign = Math.pow(-1, (x.match(/-/g) || []).length);
   let actualDigits = [...x].filter(i => i === '.' || digits.includes(i));
   let dec = actualDigits.includes('.') ? actualDigits.indexOf('.') - 1 : actualDigits.length - 1;
@@ -167,8 +198,8 @@ function stringToAutobuyerSettingNoExponent(x) {
     (a, b) => a.plus(b)) : new Decimal(1);
 }
 
-let displayDigitsAutobuyerSettings = () => NotationOptions.parseAutobuyersInCurrentBase() ? NotationOptions.displayDigits() : '0123456789';
+let displayDigitsAutobuyerSettings = (specialFormat) => specialFormat ? NotationOptions.displayDigits() : '0123456789';
 
-let exponentBaseAutobuyerSettings = () => NotationOptions.parseAutobuyersInCurrentBase() ? NotationOptions.exponentBase() : 10;
+let exponentBaseAutobuyerSettings = (specialFormat) => specialFormat ? NotationOptions.exponentBase() : 10;
 
 
