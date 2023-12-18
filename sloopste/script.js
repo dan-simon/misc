7c502b6e5dd38e177086a4681412e61bb8e68841
Dan Simon <des480@nyu.edu>
2023-12-17 21:38:59 -0500

    last remaining thing = guide

--- sloopste/script.js
+++ sloopste/script.js
@@ -25,6 +25,24 @@ class Grid {
         this.local();
     }
     
+    getColor(parity, cell, highlighted) {
+        if (this.checkerboard) {
+            return cell ? (parity ? 'black' : '#404040') :
+            (highlighted ? (parity ? '#00c000' : 'lime') : (parity ? '#c0c0c0' : ''));
+        } else {
+            return cell ? 'black' : (highlighted ? 'lime' : '');
+        }
+    }
+    
+    recolorCheckerboard() {
+        this.cells.forEach((row, rowIndex) => {
+          row.forEach((cell, colIndex) => {
+            const st = JSON.stringify([rowIndex, colIndex]);
+            document.getElementById(st).style.backgroundColor = this.getColor((rowIndex + colIndex) % 2, cell, this.highlightedSubset.has(st));
+          });
+        });
+    }
+    
     setupTable() {
         this.edgeEls = {};
         const table = document.getElementById('gridTable');
@@ -37,10 +55,10 @@ class Grid {
             const tr = document.createElement('tr');
             row.forEach((cell, colIndex) => {
                 const td = document.createElement('td');
-                td.id = JSON.stringify([rowIndex, colIndex])
-                if (cell) {
-                    td.classList.add('black');
-                } else {
+                const st = JSON.stringify([rowIndex, colIndex]);
+                td.id = st;
+                td.style.backgroundColor = this.getColor((rowIndex + colIndex) % 2, cell, this.highlightedSubset.has(st));
+                if (!cell) {
                     // Add click event listener to toggle highlighted status
                     td.addEventListener('click', this.toggleHighlightedSubsetMethod([rowIndex, colIndex]));
                     td.addEventListener('mouseover', this.hoverMethod([rowIndex, colIndex]));
@@ -149,33 +167,31 @@ class Grid {
     }
     
     toggleHighlightedSubset(cell, newVal) {
-        if (!this.cells[cell[0]][cell[1]]) {
+        if (!this.cells[cell[0]][cell[1]] && !this.state.pause) {
             const c = JSON.stringify(cell);
             const cellElement = document.getElementById(c);
             newVal ??= !this.highlightedSubset.has(c);
+            cellElement.style.backgroundColor = this.getColor((cell[0] + cell[1]) % 2, false, newVal);
             if (newVal) {
                 this.highlightedSubset.add(c);
-                cellElement.classList.add('highlighted');
             } else {
                 this.highlightedSubset.delete(c);
-                cellElement.classList.remove('highlighted');
             }
         }
     }
     
     toggleHighlightedComponent(startingCell, newVal) {
-        if (!this.cells[startingCell[0]][startingCell[1]]) {
+        if (!this.cells[startingCell[0]][startingCell[1]] && !this.state.pause) {
             newVal ??= !this.highlightedSubset.has(JSON.stringify(startingCell));
             const component = this.getConnectedComponent(startingCell);
             for (const cell of component) {
                 const c = JSON.stringify(cell);
                 const cellElement = document.getElementById(c);
+                cellElement.style.backgroundColor = this.getColor((cell[0] + cell[1]) % 2, false, newVal);
                 if (newVal) {
                     this.highlightedSubset.add(c);
-                    cellElement.classList.add('highlighted');
                 } else {
                     this.highlightedSubset.delete(c);
-                    cellElement.classList.remove('highlighted');
                 }
             }
         }
@@ -204,10 +220,13 @@ class Grid {
     }
     
     clearHighlightedSubset() {
-        this.highlightedSubset.forEach(cellStr => {
-            document.getElementById(cellStr).classList.remove('highlighted');
-        });
-        this.highlightedSubset.clear();
+        if (!this.state.pause) {
+            this.highlightedSubset.forEach(cellStr => {
+                const [rowIndex, colIndex] = JSON.parse(cellStr);
+                document.getElementById(cellStr).style.backgroundColor = this.getColor((rowIndex + colIndex) % 2, this.cells[rowIndex][colIndex], false);
+            });
+            this.highlightedSubset.clear();
+        }
     }
     
     getConnectedComponent(cell) {
@@ -433,36 +452,230 @@ class State {
       let data = this.parse(urls);
       this.unlocks = data[0];
       this.loops = data[1];
-      this.position = [0, 0, 0];
+      this.loopNames = this.loopsToLoopNames(data[1]);
+      this.position = null;
       this.jumps = 0;
+      this.levelEl = document.getElementById('level');
+      this.infoEl = document.getElementById('info');
+      this.timeEl = document.getElementById('time');
+      this.gridEl = document.getElementById('gridTable');
+      this.levelSelect = document.getElementById('levelSelect');
+      this.progress = {loops: [], cells: 0, sections: [], subsections: [], bonus: [], big: []};
+      this.setupLevelSelectButtons();
       this.setupLoop();
+      this.time = 0;
+      this.lastTick = Date.now();
+      setInterval(() => {
+        this.checkAutoSkip();
+        this.timeEl.innerText = this.timer ? this.currTime() : '';
+      }, 64);
+    }
+    
+    checkAutoSkip() {
+      if (this.autoSkip) {
+        this.skip(false);
+      }
+    }
+    
+    currTime() {
+      let now = Date.now();
+      if (this.position !== null && !this.pause) {
+        this.time += now - this.lastTick;
+      }
+      this.lastTick = now;
+      let time = this.time;
+      let parts = [Math.floor(time / 3.6e6), Math.floor(time / 6e4) % 60, Math.floor(time / 1000) % 60, time % 1000];
+      return (parts[0] ? (parts[0] + ':' + ('' + ('' + parts[1]).padStart(2, '0'))) : parts[1]) + ':' +
+      ('' + parts[2]).padStart(2, '0') + '.' + ('' + parts[3]).padStart(3, '0') + (this.pause ? ' (paused)' : '');
     }
     
     nextTimeout() {
+      this.complete();
       let jumps = this.jumps;
       setTimeout(() => {
         if (this.jumps > jumps) return;
-        this.next();
+        this.jumps++;
+        this.incrementPosition();
+        this.setupLoop();
       }, 500);
     }
     
-    next() {
+    skip(manual) {
+      if (this.pause) {
+        return;
+      }
+      let t = this.getType();
+      if (t === 'bonus' || t === 'big') {
+        this.jumps++;
+        this.completeForUnlock();
+        this.incrementPosition();
+        this.setupLoop();
+      } else if (manual) {
+        alert('Only 🍓 can be skipped!');
+      }
+    }
+    
+    quit() {
+      if (this.position === null || !confirm('Are you sure you want to return to level select?')) {
+        return;
+      }
       this.jumps++;
-      this.incrementPosition();
+      this.position = null;
       this.setupLoop();
+    }
+    
+    completeForUnlock() {
+      let id = this.position.join(',');
+      let a = this.loops[this.position[0]];
+      let b = a[this.position[1]];
+      if (this.position[1] === a.length - 1 && this.position[2] === b.length - 1 &&
+      !this.progress.sections.includes(this.position.slice(0, 1).join(','))) {
+        this.progress.sections.push(this.position.slice(0, 1).join(','));
+      }
+      if (this.position[2] === b.length - 1 &&
+      !this.progress.subsections.includes(this.position.slice(0, 2).join(','))) {
+        this.progress.subsections.push(this.position.slice(0, 2).join(','));
+      }
+    }
+    
+    complete() {
+      // this should only be called when the loop is guaranteed to be complete
+      if (this.audio) {
+        new Audio('spheal.mp3').play();
+      }
+      let id = this.position.join(',');
+      if (this.progress.loops.includes(id)) {
+        return;
+      }
+      this.progress.loops.push(id);
+      let type = this.getType();
+      if (type === 'cells') {
+        this.progress.cells += this.grid.cells.length * this.grid.cells[0].length;
+      } else if (type === 'bonus') {
+        this.progress.bonus.push(id);
+      } else if (type === 'big') {
+        this.progress.bonus.push(id);
+        this.progress.big.push(id);
+      }
+      this.completeForUnlock();
     }
     
     setupLoop() {
       if (this.getLoop() !== null) {
+        this.gridEl.style.display = '';
+        this.levelSelect.style.display = 'none';
         this.grid.initialize(this.getLoop());
+        this.levelEl.innerText = this.getLoopName();
+        this.infoEl.innerText = this.getInfo();
       } else {
-        alert('Finished!');
+        this.gridEl.style.display = 'none';
+        this.levelSelect.style.display = '';
+        this.setupLevelSelect();
+        this.levelEl.innerText = 'Main';
+        this.infoEl.innerText = this.getInfo();
+      }
+    }
+    
+    getInfo() {
+      return 'Cells: ' + this.stat(this.progress.cells) + ' ' +
+      'Chapters: ' + this.stat(this.progress.sections) + ' ' +
+      '🍓: ' + this.stat(this.progress.bonus) + ' ' +
+      '🍓🍓: ' + this.stat(this.progress.big);
+    }
+    
+    setupLevelSelectButtons() {
+      this.levelSelect.replaceChildren();
+      this.levelButtons = [];
+      for (let i = 0; i < this.loops.length; i++) {
+        let a = [];
+        let span = document.createElement('span');
+        for (let j = 0; j < this.loops[i].length; j++) {
+          let b = document.createElement('button');
+          b.innerText = this.chapterName(i, j);
+          b.onclick = this.jumpToSubsectionFunction(i, j);
+          span.appendChild(b);
+          a.push(b);
+        }
+        this.levelSelect.appendChild(span);
+        if (i !== this.loops.length - 1) {
+          this.levelSelect.appendChild(document.createElement('br'));
+        }
+        this.levelButtons.push(a);
+      }
+    }
+    
+    setupLevelSelect() {
+      for (let i = 0; i < this.levelButtons.length; i++) {
+        for (let j = 0; j < this.levelButtons[i].length; j++) {
+          if (this.isUnlocked(i, j)) {
+            this.levelButtons[i][j].style.display = '';
+            let completed = this.progress.subsections.includes([i, j].join(','));
+            let allBerries = this.loops[i][j].every((it, k) => it[1] === 'cells' || this.progress.loops.includes([i, j, k].join(',')));
+            this.levelButtons[i][j].style.backgroundColor = completed ? (allBerries ? 'lime' : 'cyan') : (allBerries ? 'yellow' : '');
+          } else {
+            this.levelButtons[i][j].style.display = 'none';
+          }
+        }
+      }
+    }
+    
+    stat(x) {
+      return Array.isArray(x) ? x.length : x;
+    }
+    
+    isUnlocked(a, b) {
+      if (b === 0) {
+        let crit = this.unlocks[a];
+        if (crit[1] === 'always') {
+          return true;
+        } else {
+          return this.stat(this.progress[crit[1]]) >= crit[0];
+        }
+      } else {
+        return this.progress.subsections.includes(a + ',' + (b - 1));
+      }
+    }
+    
+    unlockMessage(a, b) {
+      if (b === 0) {
+        if (a === 0 || this.isUnlocked(a - 1, 0)) {
+          let crit = this.unlocks[a];
+          let display = {
+            'always': 'ERROR',
+            'cells': 'cells',
+            'bonus': '🍓',
+            'big': '🍓🍓'
+          }
+          return 'Get ' + crit[0] + ' ' + display[crit[1]] + ' first.';
+        } else {
+          return 'Get ??? first';
+        }
+      } else {
+        return 'Finish ' + this.chapterName(a, b - 1) + ' first.';
+      }
+    }
+    
+    jumpToSubsectionFunction(a, b) {
+      return () => this.jumpToSubsection(a, b, false);
+    }
+    
+    jumpToSubsection(a, b, c) {
+      let res = this.isUnlocked(a, b);
+      if (res) {
+        if (c && !confirm('Are you sure you want to jump to ' + this.chapterName(a, b) + '?')) {
+          return;
+        }
+        this.jumps++;
+        this.position = [a, b, 0];
+        this.setupLoop();
+      } else {
+        alert(this.unlockMessage(a, b));
       }
     }
     
     incrementPosition() {
-      for (let i of [2, 1, 0, -1]) {
-        if (i === -1) {
+      for (let i of [2, 1, 0]) {
+        if (i === 0) {
           this.position = null;
           return;
         }
@@ -472,6 +685,10 @@ class State {
         } else {
           break;
         }
+      }
+      // Check if the new simple loop can be skipped.
+      if (this.autoSkip) {
+        this.skip(false);
       }
     }
     
@@ -486,8 +703,35 @@ class State {
         }
         loop = loop[i];
       }
-      console.log(loop[0]);
       return loop[0];
+    }
+    
+    getLoopName() {
+      if (this.position === null) {
+        return 'Level Select';
+      }
+      let loop = this.loopNames;
+      for (let i of this.position) {
+        if (i >= loop.length) {
+          return null;
+        }
+        loop = loop[i];
+      }
+      return loop;
+    }
+    
+    getType() {
+      if (this.position === null) {
+        return null;
+      }
+      let loop = this.loops;
+      for (let i of this.position) {
+        if (i >= loop.length) {
+          return null;
+        }
+        loop = loop[i];
+      }
+      return loop[1];
     }
     
     parse(x) {
@@ -498,7 +742,7 @@ class State {
     
     parseStart(x) {
       let table = {
-        'x': 'cell',
+        'x': 'cells',
         'y': 'bonus',
         'z': 'big'
       }
@@ -507,7 +751,7 @@ class State {
     
     parseLoop(x) {
       let table = {
-        'x': 'cell',
+        'x': 'cells',
         'y': 'bonus',
         'z': 'big'
       }
@@ -516,12 +760,43 @@ class State {
       let data = parts[1];
       return [urlToNestedArray('https://puzz.link/p?simpleloop/' + parts[0] + '/' + parts[0] + '/' + parts[1]), type];
     }
+    
+    chapterName(a, b) {
+      return ((a === 8) ? 10 : (a + 1)) + '-' + 'abc'[b];
+    }
+    
+    loopsToLoopNames(x) {
+      let table = {
+        'cells': '',
+        'bonus': '🍓',
+        'big': '🍓🍓'
+      };
+      let end = (l, i) => table[i] + ((i === 'big') ? '' : l.filter(it => it[1] == i).length);
+      return x.map((i, ind) => i.map((j, jnd) => j.map((k, knd) => this.chapterName(ind, jnd) + '-' + end(j.slice(0, knd + 1), k[1]))));
+    }
 }
 
 function createButton(text, onClick, key) {
     const button = document.createElement('button');
     button.innerText = text;
     button.onclick = onClick;
+    document.body.appendChild(button);
+    if (typeof key === 'string') {
+        document.addEventListener('keydown', function (event) {
+            if (event.key.toLowerCase() === key) {
+                button.click();
+            }
+        });
+    }
+}
+
+function createToggleButton(text, getBool, onClick, key) {
+    const button = document.createElement('button');
+    button.innerText = text + (getBool() ? ' (on → off)' : ' (off → on)');
+    button.onclick = function () {
+      onClick();
+      button.innerText = text + (getBool() ? ' (on → off)' : ' (off → on)');
+    }
     document.body.appendChild(button);
     if (typeof key === 'string') {
         document.addEventListener('keydown', function (event) {
@@ -547,9 +822,14 @@ window.onload = function () {
     // Initialize a grid with some cell states
     const grid = new Grid();
     grid.hoverMode = 0;
+    grid.checkerboard = false;
     grid.allowedParity = [1, 2];
     grid.generateColorParams();
     const state = new State();
+    state.audio = false;
+    state.timer = false;
+    state.autoSkip = false;
+    state.pause = false;
     state.grid = grid;
     grid.state = state;
     
@@ -559,10 +839,18 @@ window.onload = function () {
     });
     
     createButton('Clear (x)', function () {
+        if (state.pause) {
+          alert('Unpause first!');
+          return;
+        }
         grid.clearHighlightedSubset();
     }, 'x');
 
     createButton('Parity on selected region (p)', function () {
+        if (state.pause) {
+          alert('Unpause first!');
+          return;
+        }
         grid.adjustEdges(grid.highlightedSubset, true);
     }, 'p');
     
@@ -576,6 +864,13 @@ window.onload = function () {
         grid.recolor();
     }, 'z');
     
+    createToggleButton('Toggle checkerboard shading (k)', () => grid.checkerboard, function () {
+        grid.checkerboard = !grid.checkerboard;
+        if (state.position !== null) {
+          grid.recolorCheckerboard();
+        }
+    }, 'k');
+    
     let table = {
       'Mode: Default': [1, 2],
       'Mode: 1-less': [2],
@@ -585,6 +880,58 @@ window.onload = function () {
     
     createSelect(['Mode: Default', 'Mode: 1-less', 'Mode: 2-less', 'Mode: 12-less'], function (x) {
       grid.allowedParity = table[x];
+    });
+    
+    document.body.appendChild(document.createElement('br'));
+    
+    createToggleButton('Toggle audio (m)', () => state.audio, function () {
+        state.audio = !state.audio;
+    }, 'm');
+    
+    createToggleButton('Toggle timer (t)', () => state.timer, function () {
+        state.timer = !state.timer;
+    }, 't');
+    
+    document.body.appendChild(document.createElement('br'));
+    
+    createButton('Return to level select (q)', function () {
+        state.quit();
+    }, 'q');
+    
+    createButton('Skip 🍓 (s)', function () {
+        if (state.pause) {
+          alert('Unpause first!');
+          return;
+        }
+        state.skip(true);
+    }, 's');
+    
+    createToggleButton('Toggle autoskip 🍓 (d)', () => state.autoSkip, function () {
+        state.autoSkip = !state.autoSkip;
+    }, 's');
+    
+    createToggleButton('Toggle pause (space)', () => state.pause, function () {
+        state.pause = !state.pause;
+    }, ' ');
+    
+    document.addEventListener('keydown', function (event) {
+      if ('123456780'.includes(event.key)) {
+        if (state.pause) {
+          alert('Unpause first!');
+          return;
+        }
+        state.jumpToSubsection('123456780'.indexOf(event.key), 0, state.position !== null);
+      }
+      if (event.key === '9') {
+        alert('This chapter doesn\'t exist');
+      }
+      if ('abc'.includes(event.key.toLowerCase()) && this.position !== null) {
+        if (state.pause) {
+          alert('Unpause first!');
+          return;
+        }
+        state.jumpToSubsection(state.position[0], 'abc'.indexOf(event.key.toLowerCase()), state.position !== null);
+      }
     });
 }
 
