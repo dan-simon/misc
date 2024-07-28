3f403bba8d9149b9455cd69f8d92135d3bfbba0a
Dan Simon <des480@nyu.edu>
2024-07-28 15:59:14 -0400

    various changes

--- hex-clock-2/script.js
+++ hex-clock-2/script.js
@@ -1,12 +1,10 @@
 let interval = 32;
 
 let colors = [
-  x => Math.cos(Math.PI * x / Math.log2(x)),
-  x => Math.cos(Math.PI * Math.sqrt(x)),
-  x => Math.cos(Math.PI * Math.log2(x)),
+  x => Math.cos(Math.PI * x / Math.log2(Math.abs(x))),
+  x => Math.cos(Math.PI * Math.sqrt(Math.abs(x))),
+  x => Math.cos(Math.PI * Math.log2(Math.abs(x))),
 ];
-
-let useColor = true;
 
 let ln = x => Math.floor(Math.log2(x)) + x / Math.pow(2, Math.floor(Math.log2(x))) - 1
 
@@ -39,21 +37,23 @@ let hex = function (x, digits) {
     8 * dig[4 * i] + 4 * dig[4 * i + 1] + 2 * dig[4 * i + 2] + dig[4 * i + 3]]).join('');
 }
 
-let [start, unit, digits] = [0, 1, 12];
+let [start, unit, digits, useColor] = [0, 1, 12, true];
 
 let loadState = function () {
   if (localStorage.getItem('state') !== null) {
-    [start, unit, digits] = localStorage.getItem('state').split(',').map(i => +i);
+    [start, unit, digits, useColor] = localStorage.getItem('state').split(',').map(i => +i);
   }
   document.getElementById('start').value = start;
   document.getElementById('unit').value = unit;
   document.getElementById('digits').value = digits;
+  document.getElementById('colors').checked = !!useColor;
 }
 
 let updateState = function () {
   let newStart = +document.getElementById('start').value;
   let newUnit = +document.getElementById('unit').value;
   let newDigits = +document.getElementById('digits').value;
+  let newUseColor = document.getElementById('colors').checked;
   if (isFinite(newStart)) {
     start = newStart;
   }
@@ -63,18 +63,21 @@ let updateState = function () {
   if (isFinite(newDigits)) {
     digits = newDigits;
   }
-  localStorage.setItem('state', start + ',' + unit + ',' + digits);
+  useColor = newUseColor;
+  localStorage.setItem('state', start + ',' + unit + ',' + digits + ',' + (+useColor));
 }
 
 window.onload = function () {
   loadState();
   setInterval(function () {
-    let x = (Date.now() / 1000 - start) / unit;
+    let x = (Date.now() / 1000 - start) / (unit || 1);
     let num = document.getElementById('num');
     num.textContent = hex(x, digits);
     if (useColor) {
       rgb = colors.map(f => f(x));
       num.style.color = 'rgb(' + rgb.map(x => Math.round(255 * ((1 + x) / 2))).join(', ') + ')';
+    } else {
+      num.style.color = '';
     }
   }, interval);
 }
